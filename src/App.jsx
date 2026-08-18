c2cccc1f564397c5f98aef74173cc91a23f2d036
TornovDutta <tornovdutta@gmail.com>
2026-08-18 22:18:19 +0530

    addd animation in background

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,11 @@ export default function App() {
 
   return (
     <>
-      <div className="bg-gradient-mesh" aria-hidden="true"></div>
+      <div className="bg-animation" aria-hidden="true">
+        <div className="orb orb-1"></div>
+        <div className="orb orb-2"></div>
+        <div className="orb orb-3"></div>
+      </div>
 
       <Navbar />
 
